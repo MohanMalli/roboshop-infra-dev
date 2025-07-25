5fbdf2c409c6050deeebc0c6395eaeaa8ff981c6
MohanMalli <mmohan0140@gmail.com>
2025-07-25 22:15:44 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -53,6 +53,38 @@ module "mongodb" {
   vpc_id = local.vpc_id
 }
 
+module "redis" {
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
+    #source = "../../terraform-aws-securitygroup"
+    project = var.project
+    environment = var.environment
+
+  sg_name = "redis"
+  sg_description = "for redis"
+  vpc_id = local.vpc_id
+}
+
+module "mysql" {
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
+    #source = "../../terraform-aws-securitygroup"
+    project = var.project
+    environment = var.environment
+
+  sg_name = "mysql"
+  sg_description = "for mysql"
+  vpc_id = local.vpc_id
+}
+
+module "rabbitmq" {
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
+    #source = "../../terraform-aws-securitygroup"
+    project = var.project
+    environment = var.environment
+
+  sg_name = "rabbitmq"
+  sg_description = "for rabbitmq"
+  vpc_id = local.vpc_id
+}
 
 # bastion accepting connections from my laptop
 resource "aws_security_group_rule" "bastion_laptop" {
@@ -132,4 +164,37 @@ resource "aws_security_group_rule" "mongodb_vpn" {
   protocol                 = "tcp"
   source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
   security_group_id        = module.mongodb.sg_id 
+}
+
+#redis
+resource "aws_security_group_rule" "redis_vpn" {
+  count = length(var.redis_ports_vpn)
+  type                     = "ingress"
+  from_port                = var.redis_ports_vpn[count.index]
+  to_port                  = var.redis_ports_vpn[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
+  security_group_id        = module.redis.sg_id 
+}
+
+#mysql
+resource "aws_security_group_rule" "mysql_vpn" {
+  count = length(var.mysql_ports_vpn)
+  type                     = "ingress"
+  from_port                = var.mysql_ports_vpn[count.index]
+  to_port                  = var.mysql_ports_vpn[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
+  security_group_id        = module.mysql.sg_id 
+}
+
+#rabbitmq
+resource "aws_security_group_rule" "rabbitmq_vpn" {
+  count = length(var.rabbitmq_ports_vpn)
+  type                     = "ingress"
+  from_port                = var.rabbitmq_ports_vpn[count.index]
+  to_port                  = var.rabbitmq_ports_vpn[count.index]
+  protocol                 = "tcp"
+  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
+  security_group_id        = module.rabbitmq.sg_id 
 }
