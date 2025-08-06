1be7afe7e95defeddeae36dc848a56345e55f454
MohanMalli <mmohan0140@gmail.com>
2025-08-06 22:08:52 +0530

    terraform

--- 10-sg/main.tf
+++ 10-sg/main.tf
@@ -1,208 +1,156 @@
-<<<<<<< HEAD
-module "frontend" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
+ module "mongodb" {
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = var.frontend_sg_name
-  sg_description = var.frontend_sg_description
-  vpc_id = local.vpc_id
- }
-
-module "bastion" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
-    #source = "../../terraform-aws-securitygroup"
-    project = var.project
-    environment = var.environment
-
-  sg_name = var.bastion_sg_name
-  sg_description = var.bastion_sg_description
-  vpc_id = local.vpc_id
-}
-
-module "backend_alb" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
-    #source = "../../terraform-aws-securitygroup"
-    project = var.project
-    environment = var.environment
-
-  sg_name = "backend-alb"
-  sg_description = "for backend alb"
-  vpc_id = local.vpc_id
-}
-
-module "vpn" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
-    #source = "../../terraform-aws-securitygroup"
-    project = var.project
-    environment = var.environment
-
-  sg_name = "vpn"  
-  sg_description = "for vpn"
-  vpc_id = local.vpc_id
-}
-
-=======
->>>>>>> 4d20c48 (terraform)
-module "mongodb" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
-    #source = "../../terraform-aws-securitygroup"
-    project = var.project
-    environment = var.environment
-
-  sg_name = "mongodb"
-  sg_description = "for mongodb"
-  vpc_id = local.vpc_id
+    sg_name = "mongodb"
+    sg_description = "for mongodb"
+    vpc_id = local.vpc_id
 }
 
 module "redis" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "redis"
-  sg_description = "for redis"
-  vpc_id = local.vpc_id
+    sg_name = "redis"
+    sg_description = "for redis"
+    vpc_id = local.vpc_id
 }
 
 module "mysql" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "mysql"
-  sg_description = "for mysql"
-  vpc_id = local.vpc_id
+    sg_name = "mysql"
+    sg_description = "for mysql"
+    vpc_id = local.vpc_id
 }
 
 module "rabbitmq" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "rabbitmq"
-  sg_description = "for rabbitmq"
-  vpc_id = local.vpc_id
+    sg_name = "rabbitmq"
+    sg_description = "for rabbitmq"
+    vpc_id = local.vpc_id
 }
 
-<<<<<<< HEAD
-# bastion accepting connections from my laptop
-=======
 module "catalogue" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "catalogue"
-  sg_description = "for catalogue"
-  vpc_id = local.vpc_id
+    sg_name = "catalogue"
+    sg_description = "for catalogue"
+    vpc_id = local.vpc_id
 }
 
 module "user" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "user"
-  sg_description = "for user"
-  vpc_id = local.vpc_id
+    sg_name = "user"
+    sg_description = "for user"
+    vpc_id = local.vpc_id
 }
 
 module "cart" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "cart"
-  sg_description = "for cart"
-  vpc_id = local.vpc_id
+    sg_name = "cart"
+    sg_description = "for cart"
+    vpc_id = local.vpc_id
 }
 
 module "shipping" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "shipping"
-  sg_description = "for shipping"
-  vpc_id = local.vpc_id
+    sg_name = "shipping"
+    sg_description = "for shipping"
+    vpc_id = local.vpc_id
 }
 
 module "payment" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "payment"
-  sg_description = "for payment"
-  vpc_id = local.vpc_id
+    sg_name = "payment"
+    sg_description = "for payment"
+    vpc_id = local.vpc_id
 }
 
 module "backend_alb" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "backend-alb"
-  sg_description = "for backend alb"
-  vpc_id = local.vpc_id
+    sg_name = "backend-alb"
+    sg_description = "for backend alb"
+    vpc_id = local.vpc_id
 }
 
 module "frontend" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = var.frontend_sg_name
-  sg_description = var.frontend_sg_description
-  vpc_id = local.vpc_id
- }
+    sg_name = var.frontend_sg_name
+    sg_description = var.frontend_sg_description
+    vpc_id = local.vpc_id
+}
 
- module "frontend_alb" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
+module "frontend_alb" {
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "frontend-alb"
-  sg_description = "for frontend alb"
-  vpc_id = local.vpc_id
- }
+    sg_name = "frontend-alb"
+    sg_description = "for frontend alb"
+    vpc_id = local.vpc_id
+}
 
 module "bastion" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = var.bastion_sg_name
-  sg_description = var.bastion_sg_description
-  vpc_id = local.vpc_id
+    sg_name = var.bastion_sg_name
+    sg_description = var.bastion_sg_description
+    vpc_id = local.vpc_id
 }
- 
+
 module "vpn" {
-    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     #source = "../../terraform-aws-securitygroup"
+    source = "git::https://github.com/MohanMalli/terraform-aws-securitygroup.git?ref=main"
     project = var.project
     environment = var.environment
 
-  sg_name = "vpn"  
-  sg_description = "for vpn"
-  vpc_id = local.vpc_id
+    sg_name = "vpn"
+    sg_description = "for vpn"
+    vpc_id = local.vpc_id
 }
-
- 
 
 # MongoDB
 resource "aws_security_group_rule" "mongodb_vpn" {
@@ -629,47 +577,23 @@ resource "aws_security_group_rule" "frontend_alb_https" {
   security_group_id = module.frontend_alb.sg_id
 }
 
->>>>>>> 4d20c48 (terraform)
 resource "aws_security_group_rule" "bastion_laptop" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
   cidr_blocks       = ["0.0.0.0/0"]
-<<<<<<< HEAD
-  security_group_id = module.bastion.sg_id # terraform-securitygroup 
-}
-
-# backend ALB accepting connections from my bastion host on port no 80
-resource "aws_security_group_rule" "backend_alb_bastion" {
-  type                     = "ingress"
-  from_port                = 80
-  to_port                  = 80
-  protocol                 = "tcp"
-  source_security_group_id = module.bastion.sg_id  #here we placing bastion security_group_id instant of ip
-  security_group_id        = module.backend_alb.sg_id 
-}
-
-
-#VPN ports 22, 443, 1194, 943
-
-=======
   security_group_id = module.bastion.sg_id
 }
 
 #VPN ports 22, 443, 1194, 943
->>>>>>> 4d20c48 (terraform)
 resource "aws_security_group_rule" "vpn_ssh" {
   type              = "ingress"
   from_port         = 22
   to_port           = 22
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-<<<<<<< HEAD
-  security_group_id = module.vpn.sg_id # terraform-securitygroup 
-=======
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.vpn.sg_id
->>>>>>> 4d20c48 (terraform)
 }
 
 resource "aws_security_group_rule" "vpn_https" {
@@ -677,12 +601,8 @@ resource "aws_security_group_rule" "vpn_https" {
   from_port         = 443
   to_port           = 443
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-<<<<<<< HEAD
-  security_group_id = module.vpn.sg_id # terraform-securitygroup 
-=======
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.vpn.sg_id
->>>>>>> 4d20c48 (terraform)
 }
 
 resource "aws_security_group_rule" "vpn_1194" {
@@ -690,12 +610,8 @@ resource "aws_security_group_rule" "vpn_1194" {
   from_port         = 1194
   to_port           = 1194
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-<<<<<<< HEAD
-  security_group_id = module.vpn.sg_id # terraform-securitygroup 
-=======
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.vpn.sg_id
->>>>>>> 4d20c48 (terraform)
 }
 
 resource "aws_security_group_rule" "vpn_943" {
@@ -703,70 +619,8 @@ resource "aws_security_group_rule" "vpn_943" {
   from_port         = 943
   to_port           = 943
   protocol          = "tcp"
-  cidr_blocks       = ["0.0.0.0/0"]
-<<<<<<< HEAD
-  security_group_id = module.vpn.sg_id # terraform-securitygroup 
-}
-
-# backend ALB accepting connections from my VPN host on port no 80
-resource "aws_security_group_rule" "backend_alb_vpn" {
-  type              = "ingress"
-  from_port         = 80
-  to_port           = 80
-  protocol          = "tcp"
-  source_security_group_id = module.vpn.sg_id
-  security_group_id = module.backend_alb.sg_id 
-}
-
-# MongoDB
-resource "aws_security_group_rule" "mongodb_vpn" {
-  count = length(var.mongodb_ports_vpn)
-  type                     = "ingress"
-  from_port                = var.mongodb_ports_vpn[count.index]
-  to_port                  = var.mongodb_ports_vpn[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
-  security_group_id        = module.mongodb.sg_id 
-}
-
-#redis
-resource "aws_security_group_rule" "redis_vpn" {
-  count = length(var.redis_ports_vpn)
-  type                     = "ingress"
-  from_port                = var.redis_ports_vpn[count.index]
-  to_port                  = var.redis_ports_vpn[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
-  security_group_id        = module.redis.sg_id 
-}
-
-#mysql
-resource "aws_security_group_rule" "mysql_vpn" {
-  count = length(var.mysql_ports_vpn)
-  type                     = "ingress"
-  from_port                = var.mysql_ports_vpn[count.index]
-  to_port                  = var.mysql_ports_vpn[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
-  security_group_id        = module.mysql.sg_id 
-}
-
-#rabbitmq
-resource "aws_security_group_rule" "rabbitmq_vpn" {
-  count = length(var.rabbitmq_ports_vpn)
-  type                     = "ingress"
-  from_port                = var.rabbitmq_ports_vpn[count.index]
-  to_port                  = var.rabbitmq_ports_vpn[count.index]
-  protocol                 = "tcp"
-  source_security_group_id = module.vpn.sg_id # here we placing bastion security_group_id instant of ip
-  security_group_id        = module.rabbitmq.sg_id 
-}
-=======
+  cidr_blocks = ["0.0.0.0/0"]
   security_group_id = module.vpn.sg_id
 }
 
  
-
- 
- 
->>>>>>> 4d20c48 (terraform)
