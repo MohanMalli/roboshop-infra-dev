657d56ac7b38ab81c984f74777e1c283c5fded1e
MohanMalli <mmohan0140@gmail.com>
2025-08-07 19:17:28 +0530

    terraform

--- 40-databases/bootstrap.sh
+++ 40-databases/bootstrap.sh
@@ -1,9 +1,6 @@
 #!/bin/bash
 
 component=$1
-dnf install ansible -y
-<<<<<<< HEAD
-ansible-pull -U https://github.com/MohanMalli/roboshop-ansible-roles.git -e component="$1" -C main main.yaml
-=======
+dnf install ansible -y          
 ansible-pull -U https://github.com/MohanMalli/roboshop-ansible-roles-tf.git -e component=$1 -e env=$2  main.yaml
->>>>>>> 4d20c48 (terraform)
+
