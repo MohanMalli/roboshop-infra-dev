51ed9852c6dcb08a49b994ac6869bbf4bc22a8d8
MohanMalli <mmohan0140@gmail.com>
2025-08-20 17:46:24 +0530

    terraform

---
+++ 91-cdn/variables.tf
@@ -0,0 +1,15 @@
+ variable "project" {
+    default = "roboshop"
+}
+
+variable "environment" {
+    default = "dev"
+}
+
+variable "zone_id"{
+    default ="Z05130911714KBFT4KH1G"
+}
+
+variable "zone_name" {
+    default = "malli.site"
+}
